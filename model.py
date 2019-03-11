6564b72222d962f8e1acdcdcf3d8ac5874ad9ab8
吕奇峰 <lv_qi_feng@126.com>
2019-03-11 23:27:04 +0800

    F1

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torchvision.models as models
-
+import numpy as np
 
 class EncoderCNN(nn.Module):
     def __init__(self, embed_size):
@@ -23,11 +23,30 @@ class EncoderCNN(nn.Module):
 
 class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
-        pass
-    
+        super(DecoderRNN, self).__init__()
+        self.lstm = nn.LSTM(embed_size,hidden_size,num_layers,batch_first=True)
+        self.embeddings = nn.Embedding(vocab_size, embed_size)
+        self.linear = nn.Linear(hidden_size, vocab_size)
+        
     def forward(self, features, captions):
-        pass
+        captions = self.embeddings(captions)
+        embed = torch.cat((features.unsqueeze(1),captions),1)
+        r_out = self.lstm(embed)
+        output = self.linear(r_out[0])[:, :-1, :]
+        return output
 
     def sample(self, inputs, states=None, max_len=20):
-        " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
-        pass
+        #" accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
+        #pass
+        output = []
+        for i in range(max_len):
+            hiddens, states = self.lstm(inputs, states)
+            mid = self.linear(hiddens.squeeze(1))
+            predicted = mid.max(1)[1]
+            output.append(predicted.tolist()[0])
+
+            inputs = self.embeddings(predicted)
+            inputs = inputs.unsqueeze(1) 
+        #print(output)
+        #output = torch.cat(output, 1)
+        return output
